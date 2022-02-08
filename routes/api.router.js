713e5e3263105200a7f5b0ad89c2c089dc86c5f3
Becca <beccasullivan30@gmail.com>
2022-02-08 15:18:55 +0000

    create route for GET /api

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const ducksRouter = require("./ducks.router");
 const usersRouter = require("./users.router");
+const { getEndpoints } = require("../controllers/api.controllers");
 
 const apiRouter = express.Router();
 
+apiRouter.route("/").get(getEndpoints);
 apiRouter.use("/ducks", ducksRouter);
 apiRouter.use("/users", usersRouter);
 
